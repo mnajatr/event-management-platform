7091c91a1c9ce1cabda49f6905decec57a5032c0
NadhiraAmalia1 <lavinonline01@gmail.com>
2025-07-31 13:26:46 +0700

    feat(error): add app.error.ts, error.middleware.ts and not-found.middleware.ts

---
+++ server/src/middlewares/error.middleware.ts
@@ -0,0 +1,35 @@
+import logger from "../utils/logger";
+import { AppError } from "../errors/app.error.js";
+import { Request, Response, NextFunction } from "express";
+
+export function errorMiddleware(
+  error: unknown,
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  logger.error(error);
+
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      success: false,
+      message: error.message,
+    });
+  }
+
+  // Prisma errors
+  if (error && typeof error === "object" && "code" in error) {
+    if (error.code === "P2002") {
+      return response.status(400).json({
+        success: false,
+        message: "Data already exists",
+      });
+    }
+  }
+
+  // Default error
+  response.status(500).json({
+    success: false,
+    message: error instanceof Error ? error.message : "Internal server error",
+  });
+}
